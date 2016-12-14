e00d027ad02996c2760895f9272a1dd46a5860ac
joannawolthuis <jcwolthuis93@gmail.com>
2016-12-14 10:22:49 +0100

    Delete starthap.py

--- starthap.py
+++
@@ -1,9 +0,0 @@
-from haplotyper import Haplotyper
-
-if __name__ == "__main__":
-    app = Haplotyper('data/hpc/test.vcf')
-    #app.HapMaker("hgvs")
-    app.HapMaker("hgvs")
-    print app.haplotypes
-else:
-    pass
